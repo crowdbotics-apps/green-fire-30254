7ee0168e4e2fbd9b0616b6b855b0e6ed95d6a31f
Crowdbotics <team@crowdbotics.com>
2021-09-01 17:39:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "green_fire_30254Identifier",
+  url: "https://green_fire_30254Identifier.botics.co",
+  api: "https://green_fire_30254Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
